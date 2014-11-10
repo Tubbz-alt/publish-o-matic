a4ef6ec9664b2ce3e0ab979ef32466e3a243bcac
David Miller <david@deadpansincerity.com>
2014-11-10 09:55:04 +0000

    Updates to unify/obesity scrapers

--- publish/make_unify_datasets.py
+++ publish/make_unify_datasets.py
@@ -21,20 +21,21 @@ def ensure_unify_datasets_exist():
     4. Profit
     """
     dc.ensure_publisher('unify')
-    unifyfile = DATA_DIR/'unify.datasets.csv'
+    unifyfile = DATA_DIR/'datasets.csv'
     with unifyfile.csv(header=True) as csv:
         for row in csv:
-            
-            dc.Dataset.create_or_update(
-                name=slugify(row.title).lower(),
-                title=row.title,
-                state='active',
-                license_id='ogl',
-                notes=row.description,
-                url=row.url,
-                owner_org='unify',
-                resources=[]
-            )
+            if row.source == 'UNIFY2':
+                dc.Dataset.create_or_update(
+                    name=slugify(row.title).lower(),
+                    title=row.title,
+                    state='active',
+                    private=row.public=='N', 
+                    license_id='ogl',
+                    url='http://data.england.nhs.uk',
+                    owner_org='unify',
+                    resources=[]
+                )
+                print slugify(row.title).lower()
     return
 
 def main():
--- scrape/us_obesity.py
+++ scrape/us_obesity.py
@@ -2,6 +2,7 @@
 Scrape US obesity datas.
 """
 import json
+import shutil
 import sys
 import urllib
 
@@ -10,9 +11,10 @@ from lxml.html import fromstring
 import requests
 from slugify import slugify
 
-import nonsense_sessions
-
 DATA_DIR = ffs.Path.here()/'../data'
+HEALTHINDICATORS = DATA_DIR/'healthindicators'
+HEALTHINDICATORS.mkdir()
+HEALTHINDICATORS_URL = 'http://www.healthindicators.gov/Indicators/Search?Query=obesity'
 
 
 class Error(Exception): 
@@ -61,28 +63,63 @@ def health_indicators_dataset(links):
         resourceslink = base_dom.cssselect(
             '#ctl00_ctl00_ctl00_subNavigation_subNavigation_downloadLink')[0]
         resourceslink = resourceslink.get('href')
+        data={
+            'ctl00_ctl00_ctl00_ctl14_TSM': ';;System.Web.Extensions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35:en-US:4ae4914f-8c8e-4123-9d05-414d87b48357:ea597d4b:b25378d2;Telerik.Web.UI, Version=2014.1.225.40, Culture=neutral, PublicKeyToken=121fae78165ba3d4:en-US:fe3df733-ee56-4563-8789-bc399360084a:16e4e7cd:f7645509:24ee1bba:92fe8ea0:fa31b949:19620875:874f8ea2:f46195d3:490a9d4e:bd8f85e4;AjaxControlToolkit, Version=3.0.20820.16598, Culture=neutral, PublicKeyToken=28f01b0e84b6d53e:en-US:707835dd-fa4b-41d1-89e7-6df5d518ffb5:b14bb7d5:dc2d6e36:5acd2e8e:13f47f54:4cda6429:35ff259d:b80820f8:34018309;',
+                '__VIEWSTATE': base_dom.cssselect('input[name="__VIEWSTATE"]')[0].value,
+                '__VIEWSTATEGENERATOR': '53FFB31C',
+                'hiddenInputToUpdateATBuffer_CommonToolkitScripts': 1,
+                '__EVENTTARGET': 'ctl00$ctl00$ctl00$pageContent$pageContent$pageContent$btnDownload',
+                '__EVENTARGUMENT': None,
+                '__LASTFOCUS': None,
 
+                'ctl00$ctl00$ctl00$ctl15$searchTextBox': 'Search for Indicators',
+                'ctl00$ctl00$ctl00$ctl15$searchTextBoxValidatorRFVExtender_ClientState': None,
+                'ctl00$ctl00$ctl00$shareButton$urlTextBox': dataset + '/Download',
+                'ctl00_ctl00_ctl00_subNavigation_subNavigation_ctl02_ClientState': None,
+                'ctl00$ctl00$ctl00$pageContent$pageContent$pageContent$fileTypeDDL':'CSV',
+                'ctl00$ctl00$ctl00$pageContent$pageContent$pageContent$localeLevelDDL': 'National',
+                'hiddenInputToUpdateATBuffer_CommonToolkitScripts': 1
+        }
+        print data
         resp = requests.post(
             resourceslink,
-            data=nonsense_sessions.healthindicators_dot_gov_session
+            data=data,
+            stream=True
         )
+
+        if resp.status_code != 200:
+            raise Exception('Requesting dataset error')
+        if resp.headers['content-type'].startswith('text/html'):
+            import pdb; pdb.set_trace()
+            raise Exception('Not a zip file error')
+
+        import pdb; pdb.set_trace()
+        resp.raw.decode_content = True
 
         ds = Dataset(title, url, description, tags)
 
-        with ffs.Path.temp() as tmp:
-            zipfile = tmp/'download.zip'
-            zipfile << resp.content
-            zipfile = zipfile.as_zip
-            extracted = tmp/'extracted'
-            extracted.mkdir()
-            zipfile.extract(extracted)
+        tmp = ffs.Path.newdir()
+        zipfile = tmp/'download.zip'
+        
+        with zipfile.open('wb') as fh:
+            shutil.copyfileobj(resp.raw, fh)
 
-            target = DATA_DIR/slugify(title)
-            target.mkdir()
-            
-            for i in extracted.ls():
-                dest = target/i[-1]
-                moved = i.mv(dest)
+        zipfile = zipfile.as_zip
+        extracted = tmp/'extracted'
+        extracted.mkdir()
+
+        print extracted
+        print zipfile
+        
+        zipfile.extract(extracted)
+
+        target = HEALTHINDICATORS/slugify(title)
+        target.mkdir()
+
+        for i in extracted.ls():
+            print i
+            dest = target/i[-1]
+            moved = i.mv(dest)
 
         metadata = target/'metadata.json'
         metadata << ds.as_json
@@ -92,11 +129,10 @@ def health_indicators_warehouse():
     """
     Grab everything from the health indicator warehouse tagged Obesity.
     """
-    BASE_URL = 'http://www.healthindicators.gov/Indicators/Search?Query=obesity'
     print '~~~~ Fetching master dataset list from Indicator "Warehouse" ~~~~'
-    content = urllib.urlopen(BASE_URL).read()
+    content = urllib.urlopen(HEALTHINDICATORS_URL).read()
     base_dom = fromstring(content)
-    base_dom.make_links_absolute(BASE_URL)
+    base_dom.make_links_absolute(HEALTHINDICATORS_URL)
     links = [a.get('href') for a in base_dom.cssselect('.IndicatorResultsList a') 
              if a.get('href').startswith('http://')]
     health_indicators_dataset(links)
