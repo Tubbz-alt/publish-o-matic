33d7cfe08e008066ea30485574ca90276b4bb0a2
Ross Jones <ross@servercode.co.uk>
2015-03-20 10:44:15 +0000

    Change crontool to not be overzealous on scraping schedule

--- publish/tools/crontool.py
+++ publish/tools/crontool.py
@@ -36,8 +36,8 @@ def main():
         crontime = "{} {} * * * {}".format(minute, hour, cli)
         print crontime
 
-    switch = "{dms} --check 24"\
+    switch = "{dms} --check 48"\
             .format(dms=dmswitch)
     hour, minute = get_random_hours_and_minutes()
-    crontime = "0 0 * * * {}".format(switch)
+    crontime = "0 0 0/2 * * {}".format(switch)
     print crontime
